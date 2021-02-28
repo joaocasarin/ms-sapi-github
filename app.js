a49dda8388d23a5010bf097294c3a1e427bd9c0e
Joao Casarin <joaovitorcasarin@hotmail.com>
2021-02-28 04:02:11 -0300

    filtering _id and __v from /GET method

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization, Token');
     next();
 });
 
