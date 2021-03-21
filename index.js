afb46e9f8db93a184c106bdc5846e7e733baedb0
Joao Casarin <joaovitorcasarin@hotmail.com>
2021-03-21 01:37:51 -0300

    properly add cors

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 require('dotenv').config();
-
-const bodyParser = require('body-parser');
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type']
+}));
+
+/* app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization, Token');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
     next();
-});
+}); */
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 require('./src/routes/index')(app);
 
